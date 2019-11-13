175bfa28b64b464f24787d15cc9b657cacf388f8
tianxiangbing <55342775@qq.com>
2019-11-13 16:37:19 +0800

    'test'

--- dist/x-seed.js
+++ dist/x-seed.js
@@ -2022,7 +2022,21 @@ return /******/ (function(modules) { // webpackBootstrap
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
+	/*
+	 * @Descripttion: 
+	 * @Author: tianxiangbing
+	 * @Date: 2019-02-22 10:58:58
+	 * @LastEditTime: 2019-11-13 16:32:23
+	 * @github: https://github.com/tianxiangbing
+	 */
 	exports.default = {
+	    page: '页',
+	    prev: '上一页',
+	    next: '下一页',
+	    last: '最后一页',
+	    first: '第一页',
+	    go: '第',
+	    sum: '共',
 	    pageNum: '页码数'
 	};
 
--- src/local/en.js
+++ src/local/en.js
@@ -1,3 +1,17 @@
+/*
+ * @Descripttion: 
+ * @Author: tianxiangbing
+ * @Date: 2019-02-22 10:58:49
+ * @LastEditTime: 2019-11-13 16:33:08
+ * @github: https://github.com/tianxiangbing
+ */
 export default {
+    page:'page',
+    prev:'prev',
+    next:'next',
+    last:'last',
+    first:'first',
+    go:'go',
+    sum:'total',
     pageNum:'pageNum'
 }
